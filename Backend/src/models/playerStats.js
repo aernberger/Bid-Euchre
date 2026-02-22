9f14ea6118d03accc35b50c05c2e01f2d490bb27
NinaJGlick <ninaglick05@gmail.com>
2026-02-22 12:01:26 -0600

    gameResult and PlayerStats done

---
+++ Backend/src/models/playerStats.js
@@ -0,0 +1,72 @@
+class PlayerStats {
+    #userId;
+    #gamesPlayed = 0;
+    #gamesWon = 0;
+    #gamesLost = 0;
+    #totalPoints = 0;
+    #totalTricksWon = 0;
+    #bidsMade = 0;
+    #bidsWon = 0;
+
+    constructor(userId) {
+        this.#userId = userId;
+    }
+
+    recordGameResult(gameResult, playerId, playerTeamId) {
+        this.#gamesPlayed++;
+    
+        const playerWon = gameResult.winningTeamId === playerTeamId;
+    
+        if (playerWon) {
+            this.#gamesWon++;
+        } else {
+            this.#gamesLost++;
+        }
+    
+        const teamPoints = gameResult.teamScores[playerTeamId];
+        this.#totalPoints += teamPoints;
+    
+        const tricks = gameResult.playerTricks[playerId] || 0;
+        this.#totalTricksWon += tricks;
+    
+        const bid = gameResult.playerBids[playerId];
+        if (bid !== undefined) {
+            this.#bidsMade++;
+    
+            if (
+                playerId === gameResult.bidWinnerId &&
+                gameResult.contractSuccessful
+            ) {
+                this.#bidsWon++;
+            }
+        }
+    }
+
+    recordBid(success) {
+        this.#bidsMade++;
+        if (success) {
+            this.#bidsWon++;
+        }
+    }
+
+    get winPercentage() {
+        if (this.#gamesPlayed === 0) return 0;
+        return this.#gamesWon / this.#gamesPlayed;
+    }
+
+    toJSON() {
+        return {
+            userId: this.#userId,
+            gamesPlayed: this.#gamesPlayed,
+            gamesWon: this.#gamesWon,
+            gamesLost: this.#gamesLost,
+            totalPoints: this.#totalPoints,
+            totalTricksWon: this.#totalTricksWon,
+            bidsMade: this.#bidsMade,
+            bidsWon: this.#bidsWon,
+            winPercentage: this.winPercentage
+        };
+    }
+}
+
+export default PlayerStats;
