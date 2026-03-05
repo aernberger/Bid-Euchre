a04bb8a8dc19f7359843db41b356c46095dc60f3
mkaila2 <manasvikaila101@gmail.com>
2026-03-05 14:45:41 -0600

    update app

---
+++ Frontend/bideuchre-client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react";
+import PlayingCard from './components/PlayingCard.tsx';
+import WhiteBox from './components/WhiteBox.tsx';
+import { Play } from 'lucide-react';
+import Game from "./pages/game.tsx"
+
+export default function App() {
+  return (
+    <Game/>
+  );
+} 
+
