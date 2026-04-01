9ed8c2c858d167cbb5d87d13a0250b6de9d14a82
aernberger <aernberger2@huskers.unl.edu>
2026-04-01 13:50:17 -0500

    Created a test file for the contract class.

---
+++ Backend/src/tests/contractTest.ts
@@ -0,0 +1,5 @@
+import test from 'node:test';
+import assert from 'assert';
+import Suit from '../models/enums/suit.js';
+import { ContractType } from '../services/enums/contractType.js';
+import { Bid } from '../services/bid.js';
