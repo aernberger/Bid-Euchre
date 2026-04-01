cfab2ad2f0dc08386e258090591896079177b97c
NinaJGlick <ninaglick05@gmail.com>
2026-04-01 13:54:17 -0500

    added scoring logic

---
+++ Backend/src/services/scoringService.ts
@@ -0,0 +1,58 @@
+import { RoundResult } from "../models/roundResult.js";
+import Team from "../models/team.js";
+import { Contract } from "./contract.js";
+
+export class ScoringEngine {
+
+    static calculateScore(
+      contract: Contract,
+      teams: Team[],
+      teamTrickCounts: Map<number, number>
+    ): RoundResult {
+  
+      const declarerTeam = teams.find(team =>
+        team.hasPlayer(contract.declarerId)
+      );
+  
+      if (!declarerTeam) {
+        throw new Error("Declarer team not found");
+      }
+  
+      const defenderTeam = teams.find(
+        team => team.teamId !== declarerTeam.teamId
+      );
+  
+      if (!defenderTeam) {
+        throw new Error("Defender team not found");
+      }
+  
+      const declarerTricks = teamTrickCounts.get(declarerTeam.teamId) ?? 0;
+  
+      const defenderTricks =teamTrickCounts.get(defenderTeam.teamId) ?? 0;
+  
+      const contractCompleted = declarerTricks >= contract.tricksRequired;
+  
+      let pointsAwardedToTeamId: number;
+      let pointsAwarded: number;
+  
+      if (contractCompleted) {
+        pointsAwardedToTeamId = declarerTeam.teamId;
+        pointsAwarded = declarerTricks;
+      } else {
+        pointsAwardedToTeamId = declarerTeam.teamId;
+        pointsAwarded = -contract.tricksRequired;
+      }
+  
+      return new RoundResult(
+        declarerTeam.teamId,
+        defenderTeam.teamId,
+        declarerTricks,
+        defenderTricks,
+        contractCompleted,
+        pointsAwardedToTeamId,
+        pointsAwarded,
+        contract.loner,
+        contract.isMoonShot()
+      );
+    }
+  }
